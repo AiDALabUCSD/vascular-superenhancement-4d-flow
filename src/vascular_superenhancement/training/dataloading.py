1edadfc00300eda97785073de00fc68c34fd357f
Akhilesh Yeluru <ayeluru@ucsd.edu>
2025-05-20 22:39:49 -0700

    fix: sampler didnt need an example subject that would have just restricted it to sampling from that one subject. undifferentiated sampler needs to be given to queue. additionally, added separate num_workers for queue and for loader (which pushes from cpu to gpu)

--- src/vascular_superenhancement/training/dataloading.py
+++ src/vascular_superenhancement/training/dataloading.py
@@ -6,9 +6,11 @@ from torch.utils.data import DataLoader
 def build_train_loader(dataset: tio.SubjectsDataset, cfg) -> DataLoader:
     """
     Build a TorchIO patch-based DataLoader using GridSampler and Queue.
+    The GridSampler will create patches of size patch_size from each subject,
+    regardless of the subject's original shape.
     """
+    # Create a sampler that will be applied to each subject
     sampler = tio.data.GridSampler(
-        subject=dataset[0],
         patch_size=cfg.train.patch_size
     )
 
@@ -17,13 +19,15 @@ def build_train_loader(dataset: tio.SubjectsDataset, cfg) -> DataLoader:
         max_length=cfg.train.queue_length,
         samples_per_volume=cfg.train.samples_per_volume,
         sampler=sampler,
-        num_workers=cfg.train.num_workers,
+        num_workers=cfg.train.num_queue_workers,
         shuffle_subjects=True,
         shuffle_patches=True
     )
 
     loader = DataLoader(
         queue,
+        pin_memory=True,
+        num_workers=cfg.train.num_loader_workers,
         batch_size=cfg.train.batch_size
     )
 

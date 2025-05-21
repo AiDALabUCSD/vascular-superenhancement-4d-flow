7c8f9ec618477c710f8986d5f4d28c129838920f
Akhilesh Yeluru <ayeluru@ucsd.edu>
2025-05-20 22:25:01 -0700

    feat: separated preprocessing transforms from subject level augmentation transforms that are hidden behind a train boolean. additionally, transforms can be made to include or exclude certain images. in this case mag/cine data needs to be rescaled differently than flow data

--- src/vascular_superenhancement/training/transforms.py
+++ src/vascular_superenhancement/training/transforms.py
@@ -1,6 +1,6 @@
 import torchio as tio
 
-def build_transforms(cfg):
+def build_transforms(cfg, train: bool = True):
     """
     Build a TorchIO transform pipeline for 3D flow + cine data.
     Applies:
@@ -9,16 +9,36 @@ def build_transforms(cfg):
     - Spatial padding/cropping to ensure patch compatibility
     - Optional augmentations (can be added later)
     """
-    patch_size = cfg.train.patch_size  # e.g., [96, 96, 96]
     spacing = cfg.data.spacing         # e.g., [1.4, 1.4, 1.4]
 
-    transforms = tio.Compose([
+    # Preprocessing transforms
+    transforms = [
         tio.Resample(spacing),
-        tio.ZNormalization(),
-        tio.CropOrPad(patch_size),
+        tio.RescaleIntensity(out_min_max=(0, 1), include=["cine", "mag"]),
+        tio.RescaleIntensity(out_min_max=(-1, 1), include=["flow_vx", "flow_vy", "flow_vz"]),
+        # tio.ZNormalization(),
+        # tio.CropOrPad(patch_size),
         # You can add augmentations here later, like:
-        # tio.RandomFlip(axes=('LR',)),
+        # tio.RandomFlip(axes=('Left',), flip_probability=0.5),
+        # tio.RandomBlur(p=0.5),
+        # tio.RandomGhosting(p=0.5),
         # tio.RandomAffine(scales=(0.9, 1.1), degrees=10),
-    ])
+    ]
+    
+    # Subject Level Augmentation transforms
+    if train:
+        transforms += [
+            # tio.RandomAffine(scales=(0.9, 1.1), degrees=10),
+            # tio.RandomFlip(axes=('Left',), flip_probability=0.5),
+            tio.RandomBlur(p=0.5),
+            # tio.RandomGhosting(p=0.5),
+            tio.Clamp(out_min=0, out_max=1, include=["cine", "mag"]),
+            tio.Clamp(out_min=-1, out_max=1, include=["flow_vx", "flow_vy", "flow_vz"]),
+        ]
+        
+    return tio.Compose(transforms)
 
-    return transforms
+
+# how to use the transforms
+# transforms = build_transforms(cfg)
+# subject = transforms(subject)
